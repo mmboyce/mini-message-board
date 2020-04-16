5f75a190350b6bf0a79003bf158528c6c97fb4e3
mmboyce <mathieumimmsboyce@gmail.com>
2020-04-16 16:39:14 -0400

    Add messages to template.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
 
-const messages = []
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini Message Board', messages });
 });
 
 module.exports = router;
